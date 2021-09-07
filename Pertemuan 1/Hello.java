1e6ea43051f82bc348665a789c95c54f943a6097
KHILAFAH99 <imampamuji.2021@student.uny.ac.id>
2021-09-07 15:47:01 +0700

    PUSH

--- Pertemuan 1/Hello.java
+++ Pertemuan 1/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main (String[] args) {
         System.out.println("Haaii");
         System.out.println("HALOOOOOOO");
+        System.out.println("p");
     }
 }
