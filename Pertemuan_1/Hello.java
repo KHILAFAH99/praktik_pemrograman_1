92b64b7a4ad1eec4b4dd3edf39a5f5306442e22b
KHILAFAH99 <imampamuji.2021@student.uny.ac.id>
2021-09-14 13:19:50 +0700

    Menambah Komentar - Pertemuan ke -2

--- Pertemuan_1/Hello.java
+++ Pertemuan_1/Hello.java
@@ -1,9 +1,15 @@
+// Komentar di pelajar pada pertemuan ke 2
+
 public class Hello {
     public static void main (String[] args) {
         System.out.println("Haaii");
         System.out.println("HALOOOOOOO");
         System.out.println("p");
+        /*
+        ini perintah print
+        syntax comment untuk yang lebih dari 1 baris
+        -
+        */
         System.out.println("test");
-    
     }
 }
