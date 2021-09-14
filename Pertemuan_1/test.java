185e2863f0b9a6aae8a743f41cc1a6efc8904adf
KHILAFAH99 <imampamuji.2021@student.uny.ac.id>
2021-09-14 13:05:30 +0700

    up

--- Pertemuan_1/test.java
+++
@@ -1,8 +0,0 @@
-public class test {
-    public static void main (String[] args) {
-        for (int count = 1; count <= 5; count++) {
-            System.out.print(count);
-        }
-        
-    }
-}
