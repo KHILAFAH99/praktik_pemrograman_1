fb6b3fefb87242771ac3df6824a5d79b7875ff09
KHILAFAH99 <imampamuji.2021@student.uny.ac.id>
2021-09-14 12:55:56 +0700

    2

--- Pertemuan_1/Hai.py
+++ Pertemuan_1/Hai.py
@@ -1,10 +1,4 @@
-name = "imam"
-age = input("berapa?")
-if int(age) >= 17:
-    print("dewasa anda")
-
-
-print("nama saya " + name + ", saya berusia " + str(age))
+print("HAAIII")
 
 
 
---
+++ Pertemuan_1/test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main (String[] args) {
+        for (int count = 1; count <= 5; count++) {
+            System.out.print(count);
+        }
+        
+    }
+}
